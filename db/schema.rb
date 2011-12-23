087a69ef4f9c08dca6f1a9fb5b851836ffd7e728
Felipe Seixas <seixasfelipe@gmail.com>
2011-12-23 19:44:16 -0200

    referencia tabela historico corretamentew

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111223131703) do
+ActiveRecord::Schema.define(:version => 20111223213342) do
 
   create_table "ativos", :force => true do |t|
     t.date     "data"
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(:version => 20111223131703) do
     t.decimal  "volume_negocios"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "historico_ativo_id"
+    t.integer  "historico_id"
     t.integer  "prazo_termo"
   end
 
-  add_index "ativos", ["historico_ativo_id"], :name => "index_ativos_on_historico_ativo_id"
+  add_index "ativos", ["historico_id"], :name => "index_ativos_on_historico_id"
 
   create_table "historico_ativos", :force => true do |t|
     t.string   "nome_arquivo"
