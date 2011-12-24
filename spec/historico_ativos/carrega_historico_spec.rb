1c5b233b551875368b356eaa4e8887a61c6443ca
Felipe Seixas <seixasfelipe@gmail.com>
2011-12-24 00:12:50 -0200

    refatora carrega historico spec

--- spec/historico_ativos/carrega_historico_spec.rb
+++ spec/historico_ativos/carrega_historico_spec.rb
@@ -5,64 +5,64 @@ module HistoricoAtivos
 
     before(:each) do
       @file = "sample/sample_cota_hist_2003.txt"
-      @parser_h_mock = double(ParserHeader).as_null_object
-      @parser_t_mock = double(ParserTrailer).as_null_object
-      @parser_a_mock = double(ParserAtivo).as_null_object
-
-      @parser_a_mock.stub(:parse).and_return(Ativo.new)
     end
+
+    let(:parser_header) { double(ParserHeader).as_null_object }
+    let(:parser_trailer) { double(ParserTrailer).as_null_object }
+    let(:parser_ativo) {
+      p_ativo = double(ParserAtivo).as_null_object
+      p_ativo.stub(:parse).and_return(Ativo.new)
+      p_ativo
+    }
+
+    let(:loader) { CarregaHistorico.new parser_header, parser_trailer, parser_ativo }
+    let(:header) { double(Header).as_null_object }
+    let(:trailer) { double(Trailer).as_null_object }
 
     it "deveria carregar o arquivo de exemplo" do
       File.exists?(@file).should be_true
     end
 
     it "deveria parsear header uma vez" do 
-      @parser_h_mock.should_receive(:parse).once.and_return(Header.new)
+      parser_header.should_receive(:parse).once.and_return(Header.new)
 
-      loader = CarregaHistorico.new @parser_h_mock, @parser_t_mock, @parser_a_mock
       loader.load @file
     end
 
     it "deveria parsear trailer uma vez" do
-      @parser_t_mock.should_receive(:parse).once.and_return(Trailer.new)
+      parser_trailer.should_receive(:parse).once.and_return(Trailer.new)
 
-      loader = CarregaHistorico.new @parser_h_mock, @parser_t_mock, @parser_a_mock
       loader.load @file
     end
 
     it "deveria parsear ativo duas vezes" do
-      @parser_a_mock.should_receive(:parse).twice
+      parser_ativo.should_receive(:parse).twice
 
-      loader = CarregaHistorico.new @parser_h_mock, @parser_t_mock, @parser_a_mock
       loader.load @file
     end
 
     it "deveria retornar dois ativos ao carregar o arquivo" do
-      loader = CarregaHistorico.new @parser_h_mock, @parser_t_mock, ParserAtivo.new
+      loader = CarregaHistorico.new parser_header, parser_trailer, ParserAtivo.new
+
       historico = loader.load @file
+      
       historico.ativos.size.should == 2
       historico.ativos[0].codigo.should == "VALE3"
       historico.ativos[1].codigo.should == "VALE5T"
     end
 
     it "deveria importar o header ao carregar o arquivo" do
-      header = double(Header).as_null_object
       header.stub(:nome_arquivo).and_return("COTAHIST.2003")
+      parser_header.stub(:parse).and_return(header)
 
-      @parser_h_mock.stub(:parse).and_return(header)
-
-      loader = CarregaHistorico.new @parser_h_mock, @parser_t_mock, @parser_a_mock
       historico = loader.load @file
       historico.nome_arquivo.should == "COTAHIST.2003"
     end
 
     it "deveria importar o trailer ao carregar o arquivo" do
-      trailer = double(Trailer).as_null_object
       trailer.stub(:quantidade_ativos).and_return(553)
+      parser_trailer.stub(:parse).and_return(trailer)
 
-      @parser_t_mock.stub(:parse).and_return(trailer)
-
-      loader = CarregaHistorico.new @parser_h_mock, @parser_t_mock, @parser_a_mock
       historico = loader.load @file
       historico.quantidade_ativos.should == 553
     end
@@ -71,7 +71,6 @@ module HistoricoAtivos
       historico = double(Historico).as_null_object
       historico.should_receive(:save).once
 
-      loader = CarregaHistorico.new @parser_h_mock, @parser_t_mock, @parser_a_mock
       loader.persist historico
     end
   end
