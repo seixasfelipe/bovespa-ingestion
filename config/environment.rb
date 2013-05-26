0eca84e7ed9b34096e2cd2e05edd72561eb34512
Felipe Seixas <seixasfelipe@gmail.com>
2013-05-26 17:10:18 -0400

    adds APP_ENV variable and set test when using travis ci

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,14 @@
 require 'active_record'
 require 'yaml'
 
+# by default, app environment is development
+ENV['APP_ENV'] ||= 'development'
+
+
 config_path = ::File.expand_path('../../db/config.yml',  __FILE__)
 db_config = YAML.load_file(config_path)
 
 ActiveRecord::Base.establish_connection(
-  db_config['development']
+  db_config[ENV['APP_ENV']]
   )
 
