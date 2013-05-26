adf5810415d6fa427f0e19bbb5cc0744fe66d5e2
Felipe Seixas <seixasfelipe@gmail.com>
2013-05-26 18:12:15 -0400

    adds rake db:migrate to travis

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,8 @@
 require 'active_record'
 require 'yaml'
 
-p ENV['APP_ENV']
 # by default, app environment is development
 ENV['APP_ENV'] ||= 'development'
-p ENV['APP_ENV']
 
 config_path = ::File.expand_path('../../db/config.yml',  __FILE__)
 db_config = YAML.load_file(config_path)
